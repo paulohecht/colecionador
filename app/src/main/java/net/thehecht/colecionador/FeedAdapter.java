8c0f5b85baad314b51b6f90344166f746ef396a0
Paulo Hecht <thehecht@gmail.com>
2017-03-04 01:58:34 -0300

    showing list of posts

---
+++ app/src/main/java/net/thehecht/colecionador/FeedAdapter.java
@@ -0,0 +1,116 @@
+package net.thehecht.colecionador;
+
+import android.content.Context;
+import android.support.v7.util.SortedList;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+
+public class FeedAdapter extends RecyclerView.Adapter<FeedAdapter.ViewHolder> {
+
+
+    private SortedList<DataSnapshot> dataset = new SortedList<DataSnapshot>(DataSnapshot.class, new SortedList.Callback<DataSnapshot>() {
+
+        @Override
+        public int compare(DataSnapshot data1, DataSnapshot data2) {
+            return (int)(data2.child("created_at").getValue(Long.class) - data1.child("created_at").getValue(Long.class));
+        }
+
+        @Override
+        public void onInserted(int position, int count) {
+            notifyItemRangeInserted(position, count);
+        }
+
+        @Override
+        public void onRemoved(int position, int count) {
+            notifyItemRangeRemoved(position, count);
+        }
+
+        @Override
+        public void onMoved(int fromPosition, int toPosition) {
+            notifyItemMoved(fromPosition, toPosition);
+        }
+
+        @Override
+        public void onChanged(int position, int count) {
+            notifyItemRangeChanged(position, count);
+        }
+
+        @Override
+        public boolean areContentsTheSame(DataSnapshot oldItem, DataSnapshot newItem) {
+            return oldItem.equals(newItem);
+        }
+
+        @Override
+        public boolean areItemsTheSame(DataSnapshot item1, DataSnapshot item2) {
+            return item1.getKey().equals(item2.getKey());
+        }
+    });
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView textView;
+        ImageView imageView;
+
+        public ViewHolder(View v) {
+            super(v);
+            textView = (TextView) v.findViewById(R.id.text);
+            imageView = (ImageView) v.findViewById(R.id.image);
+        }
+
+        public void reset() {
+            if (textView != null) textView.setText("");
+            if (imageView != null) imageView.setImageResource(R.drawable.placeholder);
+        }
+
+        public void render(DataSnapshot data) {
+            if (textView != null) textView.setText(data.child("text").getValue(String.class));
+            if (imageView != null) ;
+
+        }
+    }
+
+    public FeedAdapter(Context context) {
+    }
+
+    public void addItem(DataSnapshot dataSnapshot) {
+        dataset.add(dataSnapshot);
+        notifyDataSetChanged();
+    }
+
+    public void removeItem(DataSnapshot dataSnapshot) {
+        dataset.remove(dataSnapshot);
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public FeedAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_main, parent, false));
+    }
+
+    @Override
+    public void onViewRecycled(ViewHolder holder) {
+        super.onViewRecycled(holder);
+        if (holder.imageView != null) holder.imageView.setImageResource(R.drawable.placeholder);
+    }
+
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.reset();
+        DataSnapshot dataSnapshot = dataset.get(position);
+        holder.render(dataSnapshot);
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataset.size();
+    }
+
+}
--- app/src/main/java/net/thehecht/colecionador/MainActivity.java
+++ app/src/main/java/net/thehecht/colecionador/MainActivity.java
@@ -5,10 +5,24 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private FeedAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +37,58 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 startActivity(new Intent(MainActivity.this, NewPostActivity.class));
+            }
+        });
+
+        adapter = new FeedAdapter(MainActivity.this);
+
+        DatabaseReference db = FirebaseDatabase.getInstance().getReference();
+
+        //Check for empty state...
+        db.child("posts").limitToLast(1).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.hasChildren()) {
+                    loadData();
+                }
+                else {
+                    //TODO: Empty state...
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void loadData() {
+        DatabaseReference db = FirebaseDatabase.getInstance().getReference("posts");
+        Query query = db.orderByChild("created_at");
+        query.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String previousChildKey) {
+                adapter.addItem(dataSnapshot);
+            }
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String previousChildKey) {
+                adapter.addItem(dataSnapshot);
+            }
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+                adapter.removeItem(dataSnapshot);
+            }
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String previousChildKey) {
+                adapter.addItem(dataSnapshot);
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
             }
         });
     }
